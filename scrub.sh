3b4ffd00b2337f3ae71b8ab0fc51bf533ba9258f
Janik Rabe <info@janikrabe.com>
2018-01-09 09:50:38 +0200

    scrub.sh: Add ylwrap

--- scrub.sh
+++ scrub.sh
@@ -2,4 +2,4 @@
 
 find . \( -name .deps -or -name Makefile -or -name Makefile.in -or -name \*.o -or -name \*.a \) -exec /bin/rm -rf {} \; 2>/dev/null
 
-/bin/rm -rf src/oidentd stamp-h1 mkinstalldirs missing install-sh depcomp configure config.sub config.status config.log config.h.in~ config.h.in config.h config.guess autom4te.cache aclocal.m4
+/bin/rm -rf src/oidentd stamp-h1 mkinstalldirs missing install-sh depcomp configure config.sub config.status config.log config.h.in~ config.h.in config.h config.guess autom4te.cache aclocal.m4 ylwrap
